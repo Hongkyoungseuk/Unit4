d49a3ea6102e8469327d2d726b037813a202eacd
{Hongkyoungseuk} <tug234@daum.net>
2023-09-18 12:52:23 +0900

    0918

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,41 @@
+
 public class Main {
     public static void main(String[] args) {
-
     }
     //_1
     public static double converLireEuro(int n) {
-        // TODO 돈을 유로(Euro)로 환전
-        return (double)n;
+        // 돈을 유로(Euro)로 환전
+        return (double)n * 0.00071;
     }
     //_2
     public static boolean sumOverflow(byte x, byte y) {
-        // TODO overflow가 일어나면 true 
-
-        return true;
+        // 두 수를 더해 overflow가 일어나면 true 
+            // x + y > Integer.MAX_VALUE;
+        if ( x > 127 - y) {
+            return true;
+        }
+        if ( x < -128 - y) {
+            return true;
+        } else {
+            return false;
+        }
     }
     //_3 TODO 직접 두개에 정수를 읽고 화면에 출력하는 프로그램을 작성하세요.
+
     //_4
     public static boolean even(long x) {
-        // TODO 숫자[x]가 짝수면 true 홀수면 false
-
-        return true;
+        // 숫자[x]가 짝수면 true 홀수면 false
+        if(x % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
     //_5
-    // TODO error가 안 생기게 하시세요
+    // error가 안 생기게 하시세요
     public static void exercise_5() {
         short x = 22;
-        byte y = x;
+        byte y = (byte)x;
         System.out.println(y);
     }
     //_6_7
@@ -47,7 +58,7 @@ public class Main {
         //_7
         // TODO 다음중 어떤것이 compiler error를 일으키는지 말하시오.
         /*
-        a = 65L;
+        s = 65L;
         f = i+100;
         i = 2*b +l;
         b1 = s;
@@ -55,13 +66,13 @@ public class Main {
         c = b;
         c = 'b';
         */
-        
+
     }
     //_8 -> Account.java , Exercise_4_8.java
     //_9 -> Product.java , TestProduct.java
     //_10
     public static char lastCharater(String s) {
-        //TODO 문자열(string) 의 마지막 문자를 return하게 작성
-        return ;
+        // 문자열(string)의 마지막 문자를 return하게 작성
+        return s.charAt(-1);
     }    
 }
--- src/Product.java
+++ src/Product.java
@@ -4,14 +4,21 @@ public class Product {
     String name;
     int number = 0;
 
-    public static void downLoad() {
-        //todo 개수(number)늘리기
+    Product (String name) {
+        this.name = name;
     }
-    public static void upLoad() {
-        //todo 개수(number)줄이기
+
+    public void downLoad(int n) {
+        // 개수(number)늘리기
+        number = number + n;
     }
-    public static void toString() {
-        //todo 제품 줄력하기(예 : "의자 , 20개"
+    public void upLoad(int n) {
+        // 개수(number)줄이기
+        number = number - n;
+    }
+    public String toString() {
+        // 제품 줄력하기(예 : "의자 , 20개"
+        return this.name;
     }
     
 }
