96af236e9cdccf7bd6c89be8d32bfa97b0fb9c5b
{Hongkyoungseuk} <tug234@daum.net>
2023-09-18 09:31:46 +0900

    0918

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
     }
+    //_1
+    public static double converLireEuro(int n) {
+        // TODO 돈을 유로로 환전
+
+        return 0;
+    }
+    //_2
+    public static boolean sumOverflow(byte x, byte y) {
+        // TODO 오버플로가 일어나면 true 
+
+        return true;
+    }
+    //_4
+    public static boolean even(long x) {
+        // TODO 숫자가 짝수면 true 홀수면 false
+
+        return true;
+    }
+    //_5
+    public static void exercise5() {
+        short x = 22;
+        byte y = x;
+        System.out.println(y);
+    }
+
+
 }
+
