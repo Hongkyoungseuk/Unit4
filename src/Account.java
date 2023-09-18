4fb0918155802467b0f02728e35f8c315166d565
{Hongkyoungseuk} <tug234@daum.net>
2023-09-18 11:07:49 +0900

    0918

--- src/Account.java
+++ src/Account.java
@@ -1,20 +1,7 @@
 //_8
-// TODO 다음을 실행할때 무엇이 출력되나요?
 public class Account {
     public int bal;
     public Account(int x) {
         bal = x;
-    }
-}
-public class Exercise_4_8 {
-    public static void method(int a, Account b) {
-        a *= 2;
-        b.bal *=2;
-    }
-    public static void main(String[] args) {
-        int c = 100;
-        Account r = new Account(100);
-        method(c, r);
-        System.out.println(c + " " + r.bal);
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -6,26 +6,12 @@ public class Product {
 
     public static void downLoad() {
         //todo 개수(number)늘리기
-        
     }
     public static void upLoad() {
         //todo 개수(number)줄이기
-
     }
     public static void toString() {
         //todo 제품 줄력하기(예 : "의자 , 20개"
     }
     
 }
-
-public class TestProduct {
-    public static void main(String[] args) {
-        Product lamp = new Product("Lamp 60 Watt");
-        System.out.println("Before the loading: " + lamp);
-        lamp.downLoad(1000);
-        lamp.upLoad(100);
-        System.out.println("After the loading: " + lamp);
-    }   
-}
-
-
